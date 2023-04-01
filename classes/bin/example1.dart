2d5dc86750561f5752da575c921e548f6bbc856a
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-04-01 21:19:31 +0200

    added exmaple 1,2 and 3

---
+++ classes/bin/example1.dart
@@ -0,0 +1,18 @@
+// Simple Classes
+void main(List<String> args) {
+  const person1 = Person(
+    name: 'John',
+    age: 20,
+  );
+  print(person1.name);
+  print(person1.age);
+}
+
+class Person {
+  final String name;
+  final int age;
+  const Person({
+    required this.name,
+    required this.age,
+  });
+}
