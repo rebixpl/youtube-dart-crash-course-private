a72e123a09a5f48eef04b37a6539ffc760aad93b
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-05-14 10:41:51 +0200

    added example 1,2

--- mixins/bin/example1.dart
+++ mixins/bin/example1.dart
@@ -1,1 +1,46 @@
+// SIMPLE MiXINS
 
+// What are mixins in dart?
+// Mixins in Dart are a way to reuse code across multiple class hierarchies without
+// having to rely on inheritance. Mixins allow a class to include methods and properties
+// from one or more other classes without having to subclass them.
+
+// Mixins are useful for sharing behavior across multiple class hierarchies, and can
+// be especially helpful in cases where multiple classes need to implement the same behavior
+// but cannot be grouped together under a common superclass. Mixins are also a powerful tool for
+// reducing code duplication and making code more modular and reusable.
+
+void main(List<String> args) {
+  final person = Person();
+  print(person.speed); // 0.0
+  person.walk(speed: 10.0); // Person is walking with speed 10.0
+  print(person.speed); // 10.0
+  person.jump(speed: 20.0); // Person is jumping with speed 20.0
+  print(person.speed); // 20.0
+}
+
+mixin HasSpeed {
+  abstract double speed;
+}
+
+mixin CanJump on HasSpeed {
+  void jump({required double speed}) {
+    print('$runtimeType is jumping with speed $speed');
+    this.speed = speed;
+  }
+}
+
+mixin CanWalk on HasSpeed {
+  void walk({required double speed}) {
+    print('$runtimeType is walking with speed $speed');
+    this.speed = speed;
+  }
+}
+
+// When it comes to mixins we say 'with' keyword
+class Person with HasSpeed, CanWalk, CanJump {
+  @override
+  double speed;
+
+  Person() : speed = 0.0;
+}
---
+++ mixins/bin/example2.dart
@@ -0,0 +1,15 @@
+// Mixins and Function Parameters
+
+void main(List<String> args) {}
+
+mixin HasFirstName {
+  String get firstName;
+}
+
+mixin HasLastName {
+  String get lastName;
+}
+
+mixin HasFullName on HasFirstName, HasLastName {
+  String get fullName => '$firstName $lastName';
+}
