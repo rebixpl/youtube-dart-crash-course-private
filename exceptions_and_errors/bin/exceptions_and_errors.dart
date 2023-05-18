1a3ad5403902896ea6ab7d1cffa43a91c325a2f9
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-05-18 14:48:19 +0200

    created exceptions_and_errors project in dart

---
+++ exceptions_and_errors/bin/exceptions_and_errors.dart
@@ -0,0 +1,5 @@
+import 'package:exceptions_and_errors/exceptions_and_errors.dart' as exceptions_and_errors;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exceptions_and_errors.calculate()}!');
+}
